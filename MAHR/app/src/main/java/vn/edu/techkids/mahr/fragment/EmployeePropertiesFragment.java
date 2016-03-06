2e2ca17ae10dc2b2f6051d9f2e482414041627ef
Huy Nguyen <qhuydtvt@gmail.com>
2016-03-06 18:57:33 +0700

    Added EmployeePropertiesFragment

--- MAHR/app/src/main/java/vn/edu/techkids/mahr/fragment/EmployeePropertiesFragment.java
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/fragment/EmployeePropertiesFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import vn.edu.techkids.mahr.R;
 
@@ -13,6 +14,9 @@ import vn.edu.techkids.mahr.R;
  * A simple {@link Fragment} subclass.
  */
 public class EmployeePropertiesFragment extends Fragment {
+
+
+    private ListView mEmployeeProperitesList;
 
 
     public EmployeePropertiesFragment() {
@@ -24,7 +28,12 @@ public class EmployeePropertiesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_properties, container, false);
+        View vRet =  inflater.inflate(R.layout.fragment_employee_properties, container, false);
+        return vRet;
+    }
+
+    private void getIntances(View vRet) {
+
     }
 
 }
