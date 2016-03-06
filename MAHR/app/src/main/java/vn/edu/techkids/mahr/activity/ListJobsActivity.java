fad7b1eba0e383ea6feb13931adf88414ba67b23
Huy Nguyen <qhuydtvt@gmail.com>
2016-03-06 20:03:01 +0700

    Added fragment transition functionality

--- MAHR/app/src/main/java/vn/edu/techkids/mahr/activity/ListJobsActivity.java
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/activity/ListJobsActivity.java
@@ -1,5 +1,8 @@
 package vn.edu.techkids.mahr.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -12,20 +15,45 @@ import vn.edu.techkids.mahr.R;
 
 public class ListJobsActivity extends AppCompatActivity {
 
+    private FragmentManager mFragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_jobs);
+
+        getIntances();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#4CAF50")));
     }
+
+    private void getIntances() {
+        mFragmentManager = getFragmentManager();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out);
         return true;
+    }
+
+    /**
+     * Open new screen
+     * @param fragment
+     * @param addToBackStack
+     */
+    public void openFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fr_job_list, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(fragment.getClass().getName());
+        }
+        fragmentTransaction.commit();
     }
 }
