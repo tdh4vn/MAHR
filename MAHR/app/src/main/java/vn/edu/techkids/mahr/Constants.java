ab54ee9db3b3804a60a04490425d16bc2d9af5b4
HungTran <tdh4vn@gmail.com>
2016-03-06 17:05:17 +0700

    add activity list jobs and adding key listener

---
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/Constants.java
@@ -0,0 +1,7 @@
+package vn.edu.techkids.mahr;
+
+/**
+ * Created by hungtran on 3/6/16.
+ */
+public class Constants {
+}
--- MAHR/app/src/main/java/vn/edu/techkids/mahr/MainActivity.java
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/MainActivity.java
@@ -1,8 +1,11 @@
 package vn.edu.techkids.mahr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		getSupportActionBar().hide();
+    }
+
+    private void initListener(){
+        Button btnIndonesia = (Button) this.findViewById(R.id.btnIndonesia);
+        Button btnVietnam = (Button) this.findViewById(R.id.btnVietnam);
+        btnIndonesia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListJobsActivity.class);
+                //intent.putExtra();
+            }
+        });
     }
 }
