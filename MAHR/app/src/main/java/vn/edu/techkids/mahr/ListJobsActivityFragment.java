b81f7294e1851a0f9a6cb8490af6da16d032ae15
HungTran <tdh4vn@gmail.com>
2016-03-06 17:10:21 +0700

    repush add activity list jobs and adding key listener

---
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/ListJobsActivityFragment.java
@@ -0,0 +1,22 @@
+package vn.edu.techkids.mahr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ListJobsActivityFragment extends Fragment {
+
+    public ListJobsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list_jobs, container, false);
+    }
+}
