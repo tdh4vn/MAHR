ba687770d40fe995d698e1fd6fa27d10c01ad5e9
Huy Nguyen <qhuydtvt@gmail.com>
2016-03-06 23:06:16 +0700

    Style property fragment

--- MAHR/app/src/main/java/vn/edu/techkids/mahr/fragment/EmployeePropertiesFragment.java
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/fragment/EmployeePropertiesFragment.java
@@ -8,14 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.zip.Inflater;
 
 import vn.edu.techkids.mahr.R;
+import vn.edu.techkids.mahr.enitity.JobProperty;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,12 +26,15 @@ import vn.edu.techkids.mahr.R;
 public class EmployeePropertiesFragment extends Fragment {
 
     private ListView mEmployeeProperitesListView;
-    private String[] mEmployeeProperites;
+    //private String[] mEmployeeProperites;
 
     private LayoutInflater mLayoutInflater;
 
+    private ArrayList<JobProperty> mJobPropertyList;
+
     public EmployeePropertiesFragment() {
         // Required empty public constructor
+        mJobPropertyList = new ArrayList<JobProperty>();
     }
 
 
@@ -37,6 +43,7 @@ public class EmployeePropertiesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vRet =  inflater.inflate(R.layout.fragment_employee_properties, container, false);
+        initData();
         getIntances(vRet);
         setupView();
         return vRet;
@@ -45,8 +52,19 @@ public class EmployeePropertiesFragment extends Fragment {
     private void getIntances(View vLayoutRoot) {
         mEmployeeProperitesListView = (ListView)vLayoutRoot.
                 findViewById(R.id.ltvEmployeePropetiesList);
-        mEmployeeProperites = getResources().getStringArray(R.array.employee_property_names);
+        //mEmployeeProperites = getResources().getStringArray(R.array.employee_property_names);
         mLayoutInflater = getActivity().getLayoutInflater();
+    }
+
+    private void initData() {
+        mJobPropertyList.clear();
+        mJobPropertyList.add(new JobProperty(getString(R.string.expertise), R.drawable.ic_build_black_24dp));
+        mJobPropertyList.add(new JobProperty(getString(R.string.age), R.drawable.ic_person_black_24dp));
+        mJobPropertyList.add(new JobProperty(getString(R.string.height), R.drawable.ic_swap_vert_black_24dp));
+        mJobPropertyList.add(new JobProperty(getString(R.string.weight), R.drawable.ic_view_module_black_24dp));
+        mJobPropertyList.add(new JobProperty(getString(R.string.language), R.drawable.ic_font_download_black_24dp));
+        mJobPropertyList.add(new JobProperty(getString(R.string.experience), R.drawable.ic_power_black_24dp));
+        mJobPropertyList.add(new JobProperty(getString(R.string.degree), R.drawable.ic_group_work_black_24dp));
     }
 
     private void setupView() {
@@ -54,12 +72,12 @@ public class EmployeePropertiesFragment extends Fragment {
 
             @Override
             public int getCount() {
-                return mEmployeeProperites.length;
+                return mJobPropertyList.size();
             }
 
             @Override
             public Object getItem(int position) {
-                return mEmployeeProperites[position];
+                return mJobPropertyList.get(position);
             }
 
             @Override
@@ -69,6 +87,7 @@ public class EmployeePropertiesFragment extends Fragment {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
+                JobProperty jobProperty = mJobPropertyList.get(position);
                 if(convertView == null) {
                     convertView = mLayoutInflater.inflate(R.layout.list_item_employee_property,
                             parent, false);
@@ -76,11 +95,15 @@ public class EmployeePropertiesFragment extends Fragment {
 
                 TextView txvEmployeeProperty = (TextView)convertView.findViewById (
                         R.id.txv_employee_property);
+                ImageView imvJobProperty = (ImageView)convertView.findViewById(
+                        R.id.imv_employee_property);
+                txvEmployeeProperty.setText(jobProperty.getPropertyName());
+                imvJobProperty.setImageResource(jobProperty.getImageId());
 
-                txvEmployeeProperty.setText(mEmployeeProperites[position]);
 
                 return convertView;
             }
         });
     }
 }
+
