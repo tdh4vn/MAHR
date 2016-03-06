79c3a975e4dec8f00f4cd3e1f9b91761904d51d7
Huy Nguyen <qhuydtvt@gmail.com>
2016-03-06 19:52:57 +0700

    Added EmployeeProperty

--- MAHR/app/src/main/java/vn/edu/techkids/mahr/fragment/EmployeePropertiesFragment.java
+++ MAHR/app/src/main/java/vn/edu/techkids/mahr/fragment/EmployeePropertiesFragment.java
@@ -1,12 +1,19 @@
 package vn.edu.techkids.mahr.fragment;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.zip.Inflater;
 
 import vn.edu.techkids.mahr.R;
 
@@ -15,9 +22,10 @@ import vn.edu.techkids.mahr.R;
  */
 public class EmployeePropertiesFragment extends Fragment {
 
+    private ListView mEmployeeProperitesListView;
+    private String[] mEmployeeProperites;
 
-    private ListView mEmployeeProperitesList;
-
+    private LayoutInflater mLayoutInflater;
 
     public EmployeePropertiesFragment() {
         // Required empty public constructor
@@ -29,11 +37,50 @@ public class EmployeePropertiesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vRet =  inflater.inflate(R.layout.fragment_employee_properties, container, false);
+        getIntances(vRet);
+        setupView();
         return vRet;
     }
 
-    private void getIntances(View vRet) {
-
+    private void getIntances(View vLayoutRoot) {
+        mEmployeeProperitesListView = (ListView)vLayoutRoot.
+                findViewById(R.id.ltvEmployeePropetiesList);
+        mEmployeeProperites = getResources().getStringArray(R.array.employee_property_names);
+        mLayoutInflater = getActivity().getLayoutInflater();
     }
 
+    private void setupView() {
+        mEmployeeProperitesListView.setAdapter(new BaseAdapter() {
+
+            @Override
+            public int getCount() {
+                return mEmployeeProperites.length;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return mEmployeeProperites[position];
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null) {
+                    convertView = mLayoutInflater.inflate(R.layout.list_item_employee_property,
+                            parent, false);
+                }
+
+                TextView txvEmployeeProperty = (TextView)convertView.findViewById(
+                        R.id.txv_employee_property);
+
+                txvEmployeeProperty.setText(mEmployeeProperites[position]);
+
+                return convertView;
+            }
+        });
+    }
 }
